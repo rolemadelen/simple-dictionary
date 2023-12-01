0e64132ca9e04e7ebed5723f24267c86f70ae38d
Madelen Yoo <rolemadelen@tuta.io>
2023-12-01 17:22:21 -0500

    simple dictionary
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

---
+++ script.js
@@ -0,0 +1,77 @@
+const BASE_URL = 'https://api.datamuse.com';
+
+async function fetchDefinition(word) {
+  const url = `${BASE_URL}/words?sp=${word}&md=d`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data[0]['defs'].slice(0, 5);
+}
+
+async function fetchSynonymOrAntonym(word, type) {
+  const url = `${BASE_URL}/words?${type}=${word}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function getSynonymOrAntonym(arr, id) {
+  const div = document.getElementById(id);
+  const ulElement = div.children[1];
+  ulElement.innerHTML = '';
+  div.classList.remove('hidden');
+
+  if (arr.length === 0) {
+    div.classList.add('hidden');
+  }
+
+  for (const data of arr) {
+    const li = document.createElement('li');
+    li.addEventListener('click', handleWordClick);
+    li.innerText = data.word;
+    ulElement.appendChild(li);
+  }
+}
+
+function fetchWordData(word) {
+  fetchDefinition(word).then((defs) => {
+    const olElement = definition.children[1];
+    olElement.innerHTML = '';
+
+    for (const def of defs) {
+      const li = document.createElement('li');
+      li.innerText = def;
+      olElement.appendChild(li);
+    }
+  });
+
+  fetchSynonymOrAntonym(word, 'rel_syn').then((syns) =>
+    getSynonymOrAntonym(syns, 'synonym')
+  );
+  fetchSynonymOrAntonym(word, 'rel_ant').then((ants) =>
+    getSynonymOrAntonym(ants, 'antonym')
+  );
+}
+
+function handleWordClick(e) {
+  const word = e.target.innerText;
+  const definition = document.getElementById('definition');
+  const headingElement = definition.children[0];
+  const textField = document.getElementById('wordInput');
+
+  textField.value = word;
+  headingElement.innerText = word;
+
+  fetchWordData(word);
+}
+
+function handleWordSearch(e) {
+  e.preventDefault();
+  const word = e.target[0].value;
+
+  const definition = document.getElementById('definition');
+  const headingElement = definition.children[0];
+  headingElement.innerText = word;
+
+  fetchWordData(word);
+}
+document.getElementById('form').addEventListener('submit', handleWordSearch);
